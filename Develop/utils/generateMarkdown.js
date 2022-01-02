9c533a7354ceb8b8de8ef8cc9809e28ab071e7e9
Shinichi <miyakawashi@gmail.com>
2022-01-02 17:36:52 -0500

    add License Badge http request

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,11 +1,20 @@
+const http = require('http')
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  label =
-  message = 
-  color = 
-  url = 'https://img.shields.io/static/v1?label=<LABEL>&message=<MESSAGE>&color=<COLOR>'
-}
+  if (!license) {return ''}
+
+  const options = {
+    host: 'img.shields.io',
+    path: '/static/v1?label=License&mesasge=${license}&color=success',
+    port: 8080 
+  }
+
+  const req = http.get(options, res => {
+
+    return (res)
+  })
+} 
 
 // TODO: Create a function that returns the license link, this will be a link to the LICENSE file in the github repository.
 // If there is no license, return an empty string
@@ -32,3 +41,5 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+
+console.log(renderLicenseBadge('MIT'));
